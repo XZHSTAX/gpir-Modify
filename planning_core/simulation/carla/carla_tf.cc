e70ea043aa5b04a1e9af1b957e9d3ed28d1fd001
xzhstax <xzhstax@foxmail.com>
2025-06-13 10:55:32 +0800

    feat(planning_core): 修改tf树，使得rviz可以正常使用
    - 为carla_tf节点添加role_name参数支持，默认值为"ego_vehicle"
    - 在main2.launch中集成keyboard_simple.launch用于简易键盘控制

--- planning_core/simulation/carla/carla_tf.cc
+++ planning_core/simulation/carla/carla_tf.cc
@@ -14,7 +14,18 @@
 int main(int argc, char* argv[]) {
   ros::init(argc, argv, "carla_tf");
   ros::NodeHandle node;
+  ros::NodeHandle private_nh("~");
   geometry_msgs::TransformStamped ego_tf;
+
+  // 从参数服务器获取role_name，默认值为"ego_vehicle"
+  std::string role_name;
+  private_nh.param<std::string>("role_name", role_name, "ego_vehicle");
+  
+  // 构建2D frame名称
+  std::string ego_2d_frame = role_name + "2d";
+  
+  ROS_INFO("carla_tf: Using role_name: %s", role_name.c_str());
+  ROS_INFO("carla_tf: 2D frame name: %s", ego_2d_frame.c_str());
 
   tf2_ros::Buffer tf_buffer;
   tf2_ros::TransformListener tf_listener(tf_buffer);
@@ -23,7 +34,7 @@ int main(int argc, char* argv[]) {
   ros::Rate rate(100);
   while (node.ok()) {
     try {
-      ego_tf = tf_buffer.lookupTransform("map", "ego_vehicle", ros::Time(0));
+      ego_tf = tf_buffer.lookupTransform("map", role_name, ros::Time(0));
       geometry_msgs::TransformStamped ego_tf_2d;
       ego_tf_2d.header.frame_id = "map";
       ego_tf_2d.header.stamp = ros::Time::now();
